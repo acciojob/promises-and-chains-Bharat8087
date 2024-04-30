fcbec2f5cb8b7038c7d512a8b4aa662cce76f3c7
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-30 13:04:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+document.getElementById("myForm").addEventListener('submit', function(event) {
+    event.preventDefault();
+    const age = document.getElementById('age').value;
+    const name = document.getElementById('name').value;
+    
+    if (!age || !name) {
+        alert("Please fill out all fields");
+        return;
+    }
+    
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (parseInt(age) >= 18) {
+                resolve(`Welcome, ${name}. You can vote.`);
+            } else {
+                reject(`Oh sorry ${name}. You aren't old enough.`);
+            }
+        }, 4000);
+    });
+    
+    promise.then((message) => {
+        alert(message);
+    }).catch((error) => {
+        alert(error);
+    });
+});
